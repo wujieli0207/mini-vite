002d37264f40fa9eb4cf6c8f34ba3aed543ac9e8
wujieli0207 <wujieli0207@outlook.com>
2023-07-30 14:40:33 +0800

    feat: 实现命令行参数和 playground 调试

---
+++ packages/mini-vite/src/node/cli.ts
@@ -0,0 +1,15 @@
+import cac from 'cac'
+
+const cli = cac()
+
+cli
+  .command('[root]', 'Run the development server')
+  .alias('serve')
+  .alias('dev')
+  .action(async () => {
+    console.log('start dev server')
+  })
+
+cli.help()
+
+cli.parse()
